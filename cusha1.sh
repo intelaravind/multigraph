af099b63b0bc6ca19655f80fab60583616a1246a
hochawa <hong.589@osu.edu>
2017-06-20 10:22:11 -0400

    o

--- cusha1.sh
+++ cusha1.sh
@@ -1,3 +1,5 @@
+#!\bin\bash
+
 nvcc entry_point.cu common/simpleTime.cu csr_src/csr_format.cu csr_src/vwc_process.cu cusha_src/cusha_format.cu cusha_src/cusha_process.cu cusha_src/find_block_size.cu common/parse_graph.cpp -O3 -DBFS -arch=sm_35 -o cusha_bfs
 nvcc entry_point.cu common/simpleTime.cu csr_src/csr_format.cu csr_src/vwc_process.cu cusha_src/cusha_format.cu cusha_src/cusha_process.cu cusha_src/find_block_size.cu common/parse_graph.cpp -O3 -DSSSP -arch=sm_35 -o cusha_sssp
 nvcc entry_point.cu common/simpleTime.cu csr_src/csr_format.cu csr_src/vwc_process.cu cusha_src/cusha_format.cu cusha_src/cusha_process.cu cusha_src/find_block_size.cu common/parse_graph.cpp -O3 -DCC -arch=sm_35 -o cusha_cc
